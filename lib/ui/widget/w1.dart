7a33253c338189e8499795e99a9d527e0c65730d
jgayon <79709433+jgayon@users.noreply.github.com>
2025-02-19 10:43:20 -0500

    Update w1.dart
    update w1

--- lib/ui/widget/w1.dart
+++ lib/ui/widget/w1.dart
@@ -1,9 +1,17 @@
 import 'package:flutter/material.dart';
 
 class W1 extends StatelessWidget {
-  const W1({super.key});
-  //final VoidCallback onAdd; // call like this: onPressed: () => onAdd(),
-  //final double value;
+  final double value;
+  final Function() addSmall;
+  final Function() subtractSmall;
+
+  const W1({
+    super.key,
+    required this.value,
+    required this.addSmall,
+    required this.subtractSmall,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -12,30 +20,33 @@ class W1 extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
+          
           Container(
             padding: const EdgeInsets.all(10),
             color: Theme.of(context).colorScheme.inversePrimary,
             child: Text(
-              '0',
+              value.toStringAsFixed(1),
               key: const Key('W1Value'),
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(10),
+          
+          Container(margin: const EdgeInsets.all(10),
             color: Theme.of(context).colorScheme.inversePrimary,
             child: Column(
               children: [
                 IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.add),
-                    key: const Key('W1Add')),
+                  onPressed: addSmall,
+                  icon: const Icon(Icons.add),
+                  key: const Key('W1Add'),
+                ),
                 IconButton(
-                    onPressed: () {},
-                    icon: const Icon(Icons.remove),
-                    key: const Key('W1Sub'))
+                  onPressed: subtractSmall,
+                  icon: const Icon(Icons.remove),
+                  key: const Key('W1Sub'),
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
