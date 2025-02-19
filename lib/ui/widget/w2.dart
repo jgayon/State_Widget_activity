80884b95291ef2763281f8539a8240865a63ea31
jgayon <79709433+jgayon@users.noreply.github.com>
2025-02-19 10:43:41 -0500

    Update w2.dart

--- lib/ui/widget/w2.dart
+++ lib/ui/widget/w2.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
 
 class W2 extends StatelessWidget {
-  const W2({super.key});
-  final double value = 0;
+  final double value;
+
+  const W2({super.key, required this.value});
+
   @override
   Widget build(BuildContext context) {
+    // Cambia el color dependiendo de la porción decimal
+    Color backgroundColor = (value - value.toInt()) < 0.4 ? Colors.grey : Colors.blueGrey;
+
     return Container(
       key: const Key('W2'),
       width: 200,
       height: 200,
-      color: Colors.blueGrey, // should be grey when the decimal is bellow 0.4
+      color: backgroundColor,
     );
   }
 }
